78d9f0ef3168d628e72679feabf2577e63b07b49
Jack Hong <jack.hong@esperie.com>
2025-07-22 12:53:12 +0800

    fix: Remove unnecessary requires_isolation marker from CSV workflow tests
    - Tests don't use mocking or modify global state
    - Removing marker fixes CI ERROR status issue
    - Tests run 2.5x faster without forking overhead
    - Other requires_isolation tests continue to work properly

--- tests/unit/workflows/test_basic_csv_workflow.py
+++ tests/unit/workflows/test_basic_csv_workflow.py
@@ -13,7 +13,6 @@ from kailash.nodes.data import CSVReaderNode, CSVWriterNode
 from kailash.runtime.local import LocalRuntime
 
 
-@pytest.mark.requires_isolation
 class TestBasicWorkflow:
     """Test basic workflow construction and execution."""
 
